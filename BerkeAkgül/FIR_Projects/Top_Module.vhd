4c02c88abec2763fe4feb90f2204ef816c8a81d5
brk9898 <40520015+brk9898@users.noreply.github.com>
2025-03-29 21:00:11 +0300

    Update Top_Module.vhd

--- BerkeAkgül/FIR_Projects/Top_Module.vhd
+++ BerkeAkgül/FIR_Projects/Top_Module.vhd
@@ -2,19 +2,26 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+use work.synchronizer_pkg.all;
+
 
 entity Top_Module is
   generic(
-        DATA_WIDTH : integer := 16  -- Data length in bits
+        c_clkfreq		: integer := 100_000_000;
+        c_baudrate		: integer := 1_000_000;  
+        DATA_WIDTH      : integer := 16  -- Data length in bits
   );
   Port(
         clk      : in  std_logic;
         reset    : in  std_logic;
-        switch   : in  std_logic_vector(2 downto 0); -- 3-bit switch sinyali                
+        
+        rx       : in  std_logic; -- 3-bit switch sinyali                
+                
         sclk     : in  std_logic;
         ss_n     : in  std_logic;
         mosi     : in  std_logic;
         miso     : out std_logic;
+        
         LED      : out std_logic_vector(15 downto 0)        
    );
 end Top_Module;
@@ -24,18 +31,18 @@ architecture Behavioral of Top_Module is
 component FIR_Filter is
     Generic (
         filter_taps  : integer := 50;
-        input_width  : integer range 8 to 25 := 16;
+        input_width  : integer range 8 to 25 := 24; 
         coeff_width  : integer range 8 to 18 := 16;
-        output_width : integer range 8 to 43 := 16  
+        output_width : integer range 8 to 43 := 43  
     );
-    Port (
-           clk    : in STD_LOGIC;
-           reset  : in STD_LOGIC;
-           enable : in STD_LOGIC;
-           switch : in std_logic_vector(2 downto 0);
-           data_i : in STD_LOGIC_VECTOR (input_width-1 downto 0);
-           data_o : out STD_LOGIC_VECTOR (output_width-1 downto 0)
-           );
+    Port ( 
+        clk          : in STD_LOGIC;
+        reset        : in STD_LOGIC;
+        enable       : in STD_LOGIC;
+        coeff_s      : in coefficients(0 to filter_taps-1); 
+        data_i       : in STD_LOGIC_VECTOR (input_width-1 downto 0);
+        data_o       : out STD_LOGIC_VECTOR (output_width-1 downto 0)
+    );
 end component;
 
 component spi_slave is
@@ -55,6 +62,19 @@ component spi_slave is
   );
 end component;
 
+component uart_rx is
+    generic (
+        c_clkfreq		: integer := 100_000_000;
+        c_baudrate		: integer := 1_000_000
+    );
+    port (
+        clk				: in std_logic;
+        rx_i			: in std_logic;
+        dout_o			: out std_logic_vector (7 downto 0);
+        rx_done_tick_o	: out std_logic
+    );
+end component;
+
 signal spi_rx_data          : std_logic_vector(DATA_WIDTH-1 downto 0);
 signal spi_rx_data_sync     : std_logic_vector(DATA_WIDTH-1 downto 0);
 signal spi_tx_data          : std_logic_vector(DATA_WIDTH-1 downto 0);
@@ -67,43 +87,123 @@ signal ENABLE_FIR           : std_logic := '0';
 signal DOUT_RX              : std_logic_vector (15 downto 0) := (others => '0');
 signal DOUT_TX              : std_logic_vector (15 downto 0) := (others => '0');
 
+signal UART_REG             : coefficients(0 to 49) := (others => (others => '0'));
+signal UART_BUFFER          : coefficients(0 to 49) := (others => (others => '0'));
+signal DOUT_UART_RX         : std_logic_vector (7 downto 0) := (others => '0');
+signal rx_done_tick         : std_logic := '0';
+signal INDEX                : integer := 0;
+signal INDEX_1              : integer := 0;
+signal rx_sync              : std_logic := '0';
+signal rx_1                 : std_logic := '0';
+
+
 type STATES is (IDLE, SPI_TX_RX_MODE, FIR_PROCESS);
 signal MSTATE : STATES := IDLE;
 
+type STATES_2 is (IDLE, UART_RX_MODE, DONE);
+signal MSTATE_2 : STATES_2 := IDLE;
 
 
 begin
 
 
 process(clk, reset) begin
+
     if reset = '1' then
-        MSTATE      <= IDLE;
-        ENABLE_FIR  <= '0';
-        RESET_FIR   <= '1';
+
+        MSTATE          <= IDLE;
+        ENABLE_FIR      <= '0';
+        RESET_FIR       <= '1';
+
+
+        MSTATE_2        <= IDLE;
+        UART_REG        <= (others => (others => '0'));
+        UART_BUFFER     <= (others => (others => '0'));
+        INDEX           <= 0;
+        INDEX_1         <= 0;
+
     elsif rising_edge(clk) then
-        RESET_FIR   <= '0';
-        spi_tx_data <= DOUT_TX;  
-        LED <= DOUT_TX;
-        DOUT_RX <= spi_rx_data_sync;
+
+        RESET_FIR       <= '0';
+        spi_tx_data     <= DOUT_TX;  
+        LED             <= DOUT_TX;
+        DOUT_RX         <= spi_rx_data_sync;
+
+        rx_sync         <= rx_1;
+        rx_1            <= rx;
 
         case MSTATE is
+
             when IDLE =>
+
                 ENABLE_FIR <= '0';
+
                 if(spi_busy_sync = '1') then
+
                     MSTATE <= SPI_TX_RX_MODE;
+
                 end if;    
+
             when SPI_TX_RX_MODE =>
+
                 if spi_busy_sync = '0' then
+
                     MSTATE <= FIR_PROCESS;
+
                 end if;
+
             when FIR_PROCESS =>
+
                 ENABLE_FIR <= '1';
                 MSTATE <= IDLE;  
+
             when others =>
+
                 MSTATE <= IDLE;
+
         end case;
+
+        case MSTATE_2 is
+
+            when IDLE =>
+        
+                if rx_done_tick = '1' then
+                    MSTATE_2 <= UART_RX_MODE;
+                end if;
+
+            when UART_RX_MODE =>
+
+                if INDEX < 50 then
+                    if INDEX_1 = 0 then
+                        UART_REG(INDEX)(7 downto 0) <= signed(DOUT_UART_RX);
+                        INDEX_1 <= INDEX_1 + 1;
+                    else
+                        UART_REG(INDEX)(15 downto 8) <= signed(DOUT_UART_RX);
+                        INDEX_1 <= 0;
+                        INDEX <= INDEX + 1;
+                    end if;
+                    INDEX <= INDEX + 1;
+                    MSTATE_2 <= IDLE;                    
+                else
+                    MSTATE_2 <= DONE;
+                end if;
+
+            when DONE =>
+
+                for i in 0 to 49 loop
+                    UART_BUFFER(i) <= UART_REG(i);
+                end loop;
+
+                MSTATE_2 <= IDLE;
+
+            when others =>
+                MSTATE_2 <= IDLE;
+
+        end case;
+
     end if;
 end process;
+
 
 SPI_Slave_Inst : entity work.spi_slave
    generic map (
@@ -159,12 +259,24 @@ generic map (
        output_width => DATA_WIDTH
 )
 port map(
-       clk    => clk,
-       reset  => RESET_FIR,
-       enable => ENABLE_FIR,
-       switch => switch,
-       data_i => DOUT_RX,
-       data_o => DOUT_TX
+       clk      => clk,
+       reset    => RESET_FIR,
+       enable   => ENABLE_FIR,
+       coeff_s  => UART_BUFFER,
+       data_i   => DOUT_RX,
+       data_o   => DOUT_TX
+);
+
+UUT_2 : entity work.uart_rx
+generic map (
+        c_clkfreq		=> c_clkfreq,
+        c_baudrate		=> c_baudrate
+)
+port map (
+        clk				=> clk,
+        rx_i			=> rx_sync,
+        dout_o			=> DOUT_UART_RX,
+        rx_done_tick_o	=> rx_done_tick
 );
 
 end Behavioral;
