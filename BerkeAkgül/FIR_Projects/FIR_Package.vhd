ae61b7f68c042a4d6d58fb777190b8bfb4752029
brk9898 <40520015+brk9898@users.noreply.github.com>
2025-03-29 20:59:04 +0300

    Create FIR_Package.vhd

---
+++ BerkeAkgül/FIR_Projects/FIR_Package.vhd
@@ -0,0 +1,9 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+package synchronizer_pkg is
+
+    type coefficients is array(natural range <>) of signed(15 downto 0);
+
+end package synchronizer_pkg;
